f2d3360b8d139baf65c91444dfc2f626c6ae3b78
Nagar <deepak.a.nagar@accenture.com>
2026-06-17 23:01:06 +0530

    feat: add test piece data to chess logic

--- src/chess_logic.py
+++ src/chess_logic.py
@@ -1,3 +1,6 @@
 class ChessLogic:
     def __init__(self):
         print("ChessLogic initialized")
+        self.piece_setup = [
+            {"piece_type": "pawn", "color": "white", "board_position": (4, 1)}
+        ]
