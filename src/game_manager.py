befa65a20f925c53767ad599e5c90f3b1e3227a2
Nagar <deepak.a.nagar@accenture.com>
2026-06-17 22:19:22 +0530

    feat: connect board to game manager

--- src/game_manager.py
+++ src/game_manager.py
@@ -1,3 +1,9 @@
+from src.board import Board
+
+from src.board import Board
+
 class GameManager:
     def __init__(self):
         print("GameManager initialized")
+        self.board = Board()
+        self.board = Board()
