a106d9f6ced383f7b7487b04e53fe39ac6eb6481
Nagar <deepak.a.nagar@accenture.com>
2026-06-17 22:59:12 +0530

    feat: connect chess logic to game manager

--- src/game_manager.py
+++ src/game_manager.py
@@ -2,10 +2,12 @@ from src.board import Board
 
 from src.board import Board
 from src.pieces import Piece
+from src.chess_logic import ChessLogic
 
 class GameManager:
     def __init__(self):
         print("GameManager initialized")
         self.board = Board()
+        self.chess_logic = ChessLogic()
         self.test_piece = Piece("pawn", "white", (3, 3))
         self.board = Board()
