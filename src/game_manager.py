7b2e965921ada1b0dc597d8e7ac71a4876531e8e
Nagar <deepak.a.nagar@accenture.com>
2026-06-17 23:02:16 +0530

    refactor: spawn test piece from chess logic data

--- src/game_manager.py
+++ src/game_manager.py
@@ -9,5 +9,12 @@ class GameManager:
         print("GameManager initialized")
         self.board = Board()
         self.chess_logic = ChessLogic()
-        self.test_piece = Piece("pawn", "white", (3, 3))
+        
+        # Spawn test piece from chess logic data
+        test_data = self.chess_logic.piece_setup[0]
+        self.test_piece = Piece(
+            test_data["piece_type"],
+            test_data["color"],
+            test_data["board_position"]
+        )
         self.board = Board()
