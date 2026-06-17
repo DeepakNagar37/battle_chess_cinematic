7116876d0b381c8219ad6ca6d59617cfbf9c6f58
Nagar <deepak.a.nagar@accenture.com>
2026-06-17 22:49:02 +0530

    feat: spawn one test piece from game manager

--- src/game_manager.py
+++ src/game_manager.py
@@ -1,9 +1,11 @@
 from src.board import Board
 
 from src.board import Board
+from src.pieces import Piece
 
 class GameManager:
     def __init__(self):
         print("GameManager initialized")
         self.board = Board()
+        self.test_piece = Piece("pawn", "white", (3, 3))
         self.board = Board()
