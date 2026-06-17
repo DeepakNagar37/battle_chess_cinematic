13410ac6e6d863bcf9a1d69c00912a389c1a18e3
Nagar <deepak.a.nagar@accenture.com>
2026-06-17 22:41:02 +0530

    feat: add single board test tile

--- src/board.py
+++ src/board.py
@@ -1,3 +1,6 @@
+from ursina import Entity, color
+
 class Board:
     def __init__(self):
         print("Board initialized")
+        Entity(model='cube', color=color.white, position=(0, 0, 0), scale=(1, 0.2, 1))
